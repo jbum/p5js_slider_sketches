d5142fe5957b970591257fc83a644183be50ecb4
Jim Bumgardner <jbum@jbum.com>
2025-07-14 22:29:24 -0700

    Pic kaleidoscope tweaks #2.

--- pic_kaleidoscope/sliders_config.js
+++ pic_kaleidoscope/sliders_config.js
@@ -1,11 +1,11 @@
 const sliders_cfg = [
     { minVal: 0, maxVal: 1, defaultVal: 0.4, name: "sides" },
-    { minVal: 0, maxVal: 1, defaultVal: 0.257, name: "blur amt" }, // was higher
+    { minVal: 0, maxVal: 1, defaultVal: 0.1, name: "blur amt" }, // was higher
     { minVal: 0, maxVal: 1, defaultVal: 0.078, name: "speed" },
-    { minVal: 0, maxVal: 1, defaultVal: 0.5, name: "M1 sides" },
-    { minVal: 0, maxVal: 1, defaultVal: 0.5, name: "M2 sides" },
+    { minVal: 0, maxVal: 1, defaultVal: 0.2, name: "M1 sides" },
+    { minVal: 0, maxVal: 1, defaultVal: 0.4, name: "M2 sides" },
     { minVal: 0, maxVal: 1, defaultVal: 0.5, name: "unused" },
-    { minVal: 0, maxVal: 1, defaultVal: 0.0, name: "feedback levels" },
+    { minVal: 0, maxVal: 1, defaultVal: 0.3, name: "feedback levels" },
     { minVal: 0, maxVal: 1, defaultVal: 0.75, name: "recurse scale" },
 ];
 
